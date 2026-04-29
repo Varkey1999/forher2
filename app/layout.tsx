3c114af1f714a66572e5875245012da1e4628c9b
Varkey1999 <33931457+Varkey1999@users.noreply.github.com>
2026-04-29 14:26:09 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "For You",
+  description: "Our special moments",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="antialiased">
+        {children}
+      </body>
+    </html>
+  );
+}
